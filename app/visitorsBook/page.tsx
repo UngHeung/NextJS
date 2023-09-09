4db66af028cec205e5be2908003dbe9d45278df3
ungHeung <t01047377477@gmail.com>
2023-09-10 01:34:55 +0900

    feat : 방명록 기능

---
+++ app/visitorsBook/page.tsx
@@ -0,0 +1,48 @@
+/**
+ * 방명록 페이지
+ */
+
+import React from "react";
+import { connectDB } from "@/utils/database";
+import "./page.css";
+import Link from "next/link";
+
+interface visitorsBookProps {
+  id: string;
+  writer: string;
+  content: string;
+  date: string;
+}
+
+const visitorsBook = async () => {
+  const client = await connectDB;
+  const db = client.db("simplepage");
+
+  const visitorsBookList: visitorsBookProps[] = await db.collection("visitorsbook").find().toArray();
+
+  return (
+    <>
+      <section className="book-wrap">
+        <h3 className="title">방명록</h3>
+        <ul className="book-list">
+          {visitorsBookList.reverse().map((item) => {
+            return (
+              <li key={item.id}>
+                <strong className="book-writer">✏️ {item.writer}</strong>
+                <span className="book-date">
+                  {`${item.date.split(" ")[0]}
+                  ${item.date.split(" ")[1]}`}
+                </span>
+                <hr className="book-line" />
+                <span className="book-content">{`${item.content}`}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section>{/* <Link href={"visitorsBook/write"}>방명록 쓰기</Link> */}</section>
+    </>
+  );
+};
+
+export default visitorsBook;
