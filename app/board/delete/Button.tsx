8f6664547345004419ae1ccdb03b5b966b453750
ungHeung <t01047377477@gmail.com>
2023-09-11 02:02:26 +0900

    fix : 메소드 delete로 변경

--- app/board/delete/Button.tsx
+++ app/board/delete/Button.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Button = (props: { _id: string }) => {
+  const router = useRouter();
+
   const handleRemove = async () => {
     try {
-      await fetch("/api/board/delete", { method: "POST", body: props._id });
+      await fetch("/api/board/delete", { method: "DELETE", body: props._id })
+        .then((res) => {
+          if (res.status === 200) {
+            return res.json();
+          } else {
+            console.log("서버에 문제 발생!");
+            return;
+          }
+        })
+        .then((res) => {
+          console.log(res);
+          router.push("/board");
+          router.refresh();
+        })
+        .catch((e) => {
+          console.error(e);
+        });
     } catch (e) {
       console.error(e);
     }
