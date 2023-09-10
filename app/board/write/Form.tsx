09fe6041b626145f5fcbcf9e64432ac8e9321be9
ungHeung <t01047377477@gmail.com>
2023-09-10 16:52:38 +0900

    feat : 글 작성 및 서버 전송 기능

--- app/board/write/Form.tsx
+++ app/board/write/Form.tsx
@@ -8,15 +8,17 @@ const Form = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   return (
-    <form action="">
+    <form action="/api/server" method="POST">
       <section className="post-input-wrap">
-        <input id="post_title" onChange={(e) => setTitle(e.target.value)} value={title} placeholder="제목"></input>
-        <textarea id="post_content" onChange={(e) => setContent(e.target.value)} value={content} placeholder="내용"></textarea>
+        <input name="title" id="post_title" onChange={(e) => setTitle(e.target.value)} value={title} placeholder="제목"></input>
+        <textarea name="content" id="post_content" onChange={(e) => setContent(e.target.value)} value={content} placeholder="내용"></textarea>
       </section>
       <section className="post-add-button-wrap">
-        <button className="post-add-submit">저장</button>
+        <button type="submit" className="post-add-submit">
+          ✔️저장
+        </button>
         <Link className="post-add-cancel" href={"/board"}>
-          취소
+          ❌취소
         </Link>
       </section>
     </form>
