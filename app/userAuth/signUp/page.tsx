216730c3ebda7560db1808c8de15127280cc4a38
ungHeung <t01047377477@gmail.com>
2023-09-12 02:19:46 +0900

    feat : 상태관리 추가

--- app/userAuth/signUp/page.tsx
+++ app/userAuth/signUp/page.tsx
@@ -2,31 +2,38 @@
  * 회원가입 페이지
  */
 
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import "./page.css";
 
 const Signup = () => {
+  const [accountName, setAccountName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
   return (
     <section className="signup-input-wrap">
       <h3 className="title">회원가입</h3>
       <form>
         <section className="user-input-wrap">
           <div>
-            <input type="text" name="accountname" id="signup_name" placeholder=" " />
-            <label htmlFor="user_name">이름</label>
+            <input type="text" name="accountname" id="signup_name" placeholder=" " onChange={(e) => setAccountName(e.target.value)} value={accountName} />
+            <label htmlFor="signup_name">이름</label>
           </div>
           <div>
-            <input type="email" name="email" id="signup_email" placeholder=" " />
-            <label htmlFor="user_email">이메일</label>
+            <input type="email" name="email" id="signup_email" placeholder=" " onChange={(e) => setEmail(e.target.value)} value={email} />
+            <label htmlFor="signup_email">이메일</label>
             <button className="button btn-normal">중복확인</button>
           </div>
           <div>
-            <input type="password" name="password" id="signup_pw" placeholder=" " />
-            <label htmlFor="user_pw">비밀번호</label>
+            <input type="password" name="password" id="signup_pw" placeholder=" " onChange={(e) => setPassword(e.target.value)} value={password} />
+            <label htmlFor="signup_pw">비밀번호</label>
           </div>
           <div>
-            <input type="password" id="pw_check" placeholder=" " />
+            <input type="password" id="pw_check" placeholder=" " onChange={(e) => setPasswordCheck(e.target.value)} value={passwordCheck} />
             <label htmlFor="pw_check">비밀번호 확인</label>
           </div>
         </section>
