e05f5fefcd3a59a5ef3f641af9ac8539a728dd56
ungHeung <t01047377477@gmail.com>
2023-09-11 16:45:16 +0900

    feat : 방명록 등록 폼

--- app/visitorsBook/write/Form.tsx
+++ app/visitorsBook/write/Form.tsx
@@ -1,30 +1,49 @@
+/**
+ * 방명록 폼 컴포넌트
+ */
+
+"use client";
+
 import "./Form.css";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Form = () => {
+  const [writer, setWriter] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    console.log(writer, content);
+  }, [writer, content]);
+
   const getDate = () => {
     const date = new Date();
-    let result = `${date.getFullYear()}-${date.getMonth() - 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    let result = `${year}-${month}-${day} ${hour}:${minute}`;
     return result;
   };
 
   return (
-    <form id="book_write_form" action={""}>
+    <form id="book_write_form" action={"/api/visitorsbook/post"} method="POST">
       <header className="book-write-head">
         <div>
           <label className="book-writer-input" htmlFor="book_writer_input">
             작성자명
           </label>
-          <input id="book_writer_input" name="writer" type="text" placeholder="이름" />
+          <input id="book_writer_input" name="writer" type="text" placeholder="이름" onChange={(e) => setWriter(e.target.value)} value={writer} />
         </div>
         <input id="book_date_input" name="date" type="text" defaultValue={getDate()} />
-        <button type="submit" className="book-write-submit">
+        <button type="submit" className="btn-basic">
           등록
         </button>
       </header>
       <section>
-        <textarea className="book-content-area" placeholder="방명록을 작성해주세요"></textarea>
+        <textarea name="content" className="book-content-area" placeholder="방명록을 작성해주세요" onChange={(e) => setContent(e.target.value)} value={content}></textarea>
       </section>
     </form>
   );
