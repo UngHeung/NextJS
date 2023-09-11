9f9a8de3935f2f531e8af6ee759e17a34283f5ba
ungHeung <t01047377477@gmail.com>
2023-09-11 21:14:42 +0900

    refactor : 헤더, 푸터 컴포넌트화 및 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Link from "next/link";
+import { getServerSession } from "next-auth";
 import "./layout.css";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,33 +14,15 @@ export const metadata: Metadata = {
   description: "Next.js 실습",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  let session = await getServerSession(authOptions);
+
   return (
     <html lang="ko-KR">
       <body className={inter.className}>
-        <header className="main-header">
-          <div className="main-header-wrap">
-            <h2>
-              <Link href={"/"}>DEV.</Link>
-            </h2>
-            <nav className="main-nav">
-              <ul className="main-menu">
-                <li>
-                  <Link href={"/board"}>게시판</Link>
-                </li>
-                <li>
-                  <Link href={"/visitorsBook"}>방명록</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </header>
+        <Header user={session?.user} />
         <main className="main">{children}</main>
-        <footer className="main-footer">
-          <div className="main-footer-wrap">
-            <h2>Next.js 연습하기</h2>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
