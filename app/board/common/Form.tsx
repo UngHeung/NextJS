4a41ce33867a3865db1c715948436e33e7f39cf3
ungHeung <t01047377477@gmail.com>
2023-09-13 17:54:33 +0900

    feat : 게시물 작성시 유저 아이디, 이름 함께 저장

--- app/board/common/Form.tsx
+++ app/board/common/Form.tsx
@@ -10,16 +10,23 @@ import Link from "next/link";
 import { PostProps } from "@/utils/interface/boardInterface";
 
 type PostReqType = "write" | "update";
-interface BoardFormOption {
+interface BoardFormProps {
   type: PostReqType;
+  userdata: userProps;
   data?: PostProps;
 }
+export interface userProps {
+  _id: string;
+  name: string;
+  email: string;
+}
 
-const Form = ({ ...props }: BoardFormOption) => {
+const Form = ({ ...props }: BoardFormProps) => {
   const type = props.type;
   const data = props.data;
   const _id = data?._id;
-  const writerid = data?.writerid;
+  const writerid = props?.userdata?._id;
+  const writer = props?.userdata?.name;
 
   const [title, setTitle] = useState(type === "write" ? "" : data?.title);
   const [content, setContent] = useState(type === "write" ? "" : data?.content);
@@ -29,6 +36,12 @@ const Form = ({ ...props }: BoardFormOption) => {
     <form action={type === "write" ? "/api/board/post" : "/api/board/update"} method="POST">
       <section className="post-input-wrap">
         {type === "update" && <input name="_id" type="text" defaultValue={_id} style={{ display: "none" }} />}
+        {type === "write" && (
+          <>
+            <input name="writerid" type="text" defaultValue={writerid} style={{ display: "none" }} />
+            <input name="writer" type="text" defaultValue={writer} style={{ display: "none" }} />
+          </>
+        )}
         <input name="title" id="post_title" onChange={(e) => setTitle(e.target.value)} value={title} placeholder="제목"></input>
         <textarea name="content" id="post_content" onChange={(e) => setContent(e.target.value)} value={content} placeholder="내용"></textarea>
       </section>
