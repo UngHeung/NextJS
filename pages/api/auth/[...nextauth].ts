98bbad07a94f08e457c3f52f2bd87e677498c068
ungHeung <t01047377477@gmail.com>
2023-09-13 16:07:55 +0900

    style : 불필요 코드 제거

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,15 @@
 import { OAUTH } from "@/utils/authConfig";
 import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectDB } from "@/utils/database";
-import { NextAuthOptions, User } from "next-auth";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: OAUTH.GITHUB.ID,
-      clientSecret: OAUTH.GITHUB.PW,
-    }),
+    // GithubProvider({
+    //   clientId: OAUTH.GITHUB.ID,
+    //   clientSecret: OAUTH.GITHUB.PW,
+    // }),
     CredentialsProvider({
       name: "",
       credentials: {
@@ -22,14 +20,13 @@ export const authOptions: NextAuthOptions = {
         const client = await connectDB;
         const db = client.db("simplepage");
         const user = await db.collection("userauth").findOne({ email: credentials?.email });
-        console.log(user);
 
         if (!user) {
-          console.log("이메일 없음");
+          console.log("가입되지 않은 이메일");
           return null;
         }
 
-        if (!credentials?.password === user.password) {
+        if (credentials?.password !== user.password) {
           console.log("잘못된 비밀번호");
           return null;
         }
@@ -41,7 +38,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
     // maxAge: 30 * 24 * 60 * 60,
-    maxAge: 10,
+    maxAge: 60 * 60,
   },
 
   callbacks: {
@@ -62,10 +59,8 @@ export const authOptions: NextAuthOptions = {
 
   pages: {
     signIn: "/userAuth",
-    signOut: "/",
   },
 
-  adapter: MongoDBAdapter(connectDB),
   secret: OAUTH.SECRET,
 };
 
