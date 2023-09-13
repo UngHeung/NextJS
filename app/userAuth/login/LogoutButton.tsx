bd650602f39e2ff2deae2b6f120ebdfedbdc4cb6
ungHeung <t01047377477@gmail.com>
2023-09-13 23:23:55 +0900

    feat : 로그아웃 버튼

---
+++ app/userAuth/login/LogoutButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const LogoutButton = () => {
+  const router = useRouter();
+
+  return (
+    <button
+      className="button btn-delete"
+      onClick={() => {
+        signOut();
+        router.refresh();
+        router.push("/");
+      }}
+    >
+      로그아웃
+    </button>
+  );
+};
+
+export default LogoutButton;
