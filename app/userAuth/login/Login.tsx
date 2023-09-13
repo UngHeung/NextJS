093c7c4b7ce0d8002a4e9b8139bb2850b239597d
ungHeung <t01047377477@gmail.com>
2023-09-13 14:57:26 +0900

    feat : 이메일 로그인 구현

--- app/userAuth/login/Login.tsx
+++ app/userAuth/login/Login.tsx
@@ -2,44 +2,56 @@
  * 로그인 컴포넌트
  */
 
-import Link from "next/link";
-import React from "react";
-import { getServerSession } from "next-auth";
-import OauthLogin from "../oauth/OauthLogin";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+"use client";
 
-const Login = async () => {
-  let session = await getServerSession(authOptions);
-  const user = session?.user;
+import Link from "next/link";
+import React, { FormEvent, useState } from "react";
+import OauthLogin from "../oauth/OauthLogin";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>, email: string, password: string) => {
+    e.preventDefault();
+    await signIn("credentials", {
+      email: email,
+      password: password,
+      redirect: false,
+    }).then((res) => {
+      if (res?.ok) {
+        router.refresh();
+        router.push("/");
+      }
+    });
+  };
+
   return (
-    <>
-      {!user ? (
-        <form action={"/api/auth/get"} method="GET">
-          <section className="sns-login-wrap">
-            <OauthLogin isAuth={false} />
-          </section>
-          <hr className="login-quarter-line" />
-          <section className="id-login-wrap">
-            <div>
-              <input name="email" type="email" id="user_id" placeholder=" " />
-              <label htmlFor="user_id">이메일</label>
-            </div>
-            <div>
-              <input name="password" type="password" id="user_pw" placeholder=" " />
-              <label htmlFor="user_pw">비밀번호</label>
-            </div>
-          </section>
-          <section className="signin-button-wrap">
-            <button className="button btn-login">로그인</button>
-            <Link className="button btn-normal" href={"userAuth/signUp"}>
-              회원가입
-            </Link>
-          </section>
-        </form>
-      ) : (
-        <OauthLogin isAuth={true} />
-      )}
-    </>
+    <form onSubmit={(e) => handleSubmit(e, email, password)} method="GET">
+      <section className="sns-login-wrap">
+        <OauthLogin isAuth={false} />
+      </section>
+      <hr className="login-quarter-line" />
+      <section className="id-login-wrap">
+        <div>
+          <input name="email" type="email" id="user_id" placeholder=" " onChange={(e) => setEmail(e.target.value)} value={email} />
+          <label htmlFor="user_id">이메일</label>
+        </div>
+        <div>
+          <input name="password" type="password" id="user_pw" placeholder=" " onChange={(e) => setPassword(e.target.value)} value={password} />
+          <label htmlFor="user_pw">비밀번호</label>
+        </div>
+      </section>
+      <section className="signin-button-wrap">
+        <button className="button btn-login">로그인</button>
+        <Link className="button btn-normal" href={"userAuth/signUp"}>
+          회원가입
+        </Link>
+      </section>
+    </form>
   );
 };
 
