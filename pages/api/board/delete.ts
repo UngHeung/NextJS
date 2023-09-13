8c27fa96fd065affb032f20483211fad3a552e7e
ungHeung <t01047377477@gmail.com>
2023-09-13 21:39:44 +0900

    fix : 게시물 삭제 안되는 현상 수정

--- pages/api/board/delete.ts
+++ pages/api/board/delete.ts
@@ -7,10 +7,12 @@ import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const body = JSON.parse(req.body);
+  console.log(body);
   try {
     const client = await connectDB;
     const db = client.db("simplepage");
-    await db.collection("board").deleteOne({ _id: new ObjectId(req.body) });
+    await db.collection("board").deleteOne({ _id: new ObjectId(body._id) });
     return res.status(200).json("성공");
   } catch (e) {
     console.error(e);
