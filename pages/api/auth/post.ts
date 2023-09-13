e46af2287ba194b68992fdf2ce7376f30e8586d3
ungHeung <t01047377477@gmail.com>
2023-09-13 15:01:22 +0900

    style : 불필요 코드 제거

--- pages/api/auth/post.ts
+++ pages/api/auth/post.ts
@@ -3,7 +3,6 @@
  */
 
 import { connectDB } from "@/utils/database";
-// import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -13,14 +12,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     } else if (!req.body.password) {
       return res.status(500).json("비밀번호 미입력");
     }
-    // // const hash = await bcrypt.hash(req.body.password, 10);
-    // req.body.password = hash;
 
     const client = await connectDB;
     const db = client.db("simplepage");
 
     await db.collection("userauth").insertOne(req.body);
-    // res.status(200).json("성공");
     res.redirect(302, "/userAuth");
   }
 };
