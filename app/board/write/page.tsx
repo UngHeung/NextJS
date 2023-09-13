2654776c97abef25a04f7301322888fc73512c7e
ungHeung <t01047377477@gmail.com>
2023-09-13 17:58:38 +0900

    feat : 로그인중인 사용자 세션 데이터 추가

--- app/board/write/page.tsx
+++ app/board/write/page.tsx
@@ -4,10 +4,20 @@
 
 import { connectDB } from "@/utils/database";
 import React from "react";
-import Form from "../common/Form";
+import Form, { userProps } from "../common/Form";
 import "../page.css";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
 
-const postWrite = () => {
+const postWrite = async () => {
+  const session = await getServerSession(authOptions);
+  const user = session?.user as userProps;
+  if (!user) {
+    redirect("/userAuth");
+  }
+
+  console.log(session?.user);
   const writer = "관리자";
   let no: number;
   const handleSubmit = async () => {
@@ -21,7 +31,7 @@ const postWrite = () => {
       <main className="board-write-main">
         <h3 className="title">글쓰기</h3>
         <header className="board-write-head">
-          <Form type="write" />
+          <Form type="write" userdata={user} />
         </header>
       </main>
     </>
