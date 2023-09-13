a566f57ede6b16f984488e1dfa77d54e493599ab
ungHeung <t01047377477@gmail.com>
2023-09-13 19:43:26 +0900

    feat : 비회원 방명록 비밀번호 입력, 작성자 아이디 전송

--- app/visitorsBook/write/Form.tsx
+++ app/visitorsBook/write/Form.tsx
@@ -4,13 +4,19 @@
 
 "use client";
 
+import { userProps } from "@/app/board/common/Form";
 import "./Form.css";
 
 import React, { useState } from "react";
 
-export const Form = () => {
-  const [writer, setWriter] = useState("");
+interface visitorsBookProps {
+  userdata: userProps;
+}
+
+export const Form = ({ userdata }: visitorsBookProps) => {
+  const [writer, setWriter] = useState(userdata?.name);
   const [content, setContent] = useState("");
+  const [bookPassword, setBookPassword] = useState("");
 
   const getDate = () => {
     const date = new Date();
@@ -28,10 +34,24 @@ export const Form = () => {
     <form id="book_write_form" action={"/api/visitorsBook/post"} method="POST">
       <header className="book-write-head">
         <div>
+          {userdata && (
+            <>
+              <input type="text" id="" name="writerid" defaultValue={userdata._id} style={{ display: "none" }} />
+              <input type="text" name="authtype" defaultValue={"true"} style={{ display: "none" }} />
+            </>
+          )}
           <label className="book-writer-input" htmlFor="book_writer_input">
             작성자명
           </label>
-          <input id="book_writer_input" name="writer" type="text" placeholder="이름" onChange={(e) => setWriter(e.target.value)} value={writer} />
+          <input id="book_writer_input" name="writer" type="text" placeholder="이름" onChange={(e) => setWriter(e.target.value)} defaultValue={writer} readOnly={userdata ? true : false} />
+          {!userdata && (
+            <>
+              <label className="book-password-input" htmlFor="book_password_input">
+                비밀번호
+              </label>
+              <input id="book_password_input" name="password" type="password" placeholder="비밀번호" onChange={(e) => setBookPassword(e.target.value)} />
+            </>
+          )}
         </div>
         <input id="book_date_input" name="date" type="text" defaultValue={getDate()} />
         <button type="submit" className="button btn-normal">
