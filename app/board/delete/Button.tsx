1312358c6c136a25221c645f590bfaa1b3fe6bcb
ungHeung <t01047377477@gmail.com>
2023-09-13 19:41:23 +0900

    feat : 비회원 방명록 삭제 비밀번호 입력 기능

--- app/board/delete/Button.tsx
+++ app/board/delete/Button.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import { userProps } from "../common/Form";
+import "./Button.css";
 
-const Button = (props: { _id: string; req: string }) => {
+const Button = (props: { _id: string; userdata: userProps; req: string; authtype: boolean }) => {
   const router = useRouter();
+  const [bookPassword, setBookPassword] = useState("");
 
   const handleRemove = async () => {
     try {
-      await fetch(`/api/${props.req}/delete`, { method: "DELETE", body: props._id })
+      await fetch(`/api/${props.req}/delete`, { method: "DELETE", body: JSON.stringify({ _id: props._id, bookpassword: bookPassword, userdata: props.userdata }) })
         .then((res) => {
           if (res.status === 200) {
             return res.json();
@@ -30,9 +33,12 @@ const Button = (props: { _id: string; req: string }) => {
     }
   };
   return (
-    <button id="board_delete_button" className="button btn-delete" onClick={handleRemove}>
-      삭제
-    </button>
+    <>
+      {!props.authtype && <input id="book_delete_input" type="password" name="bookpassword" onChange={(e) => setBookPassword(e.target.value)} value={bookPassword} placeholder="비밀번호" />}
+      <button id="book_delete_button" className="button btn-delete" onClick={handleRemove}>
+        삭제
+      </button>
+    </>
   );
 };
 
