b39c8d5a37b32fc27a5fc0838ef9567ab91e9347
ungHeung <t01047377477@gmail.com>
2023-09-14 18:11:54 +0900

    feat : 삭제 종류 타입 추가

--- utils/interface/visitorsBook/visitorsbookInterfaces.ts
+++ utils/interface/visitorsBook/visitorsbookInterfaces.ts
@@ -2,6 +2,8 @@
  * 방명록 인터페이스
  */
 
+export type DeleteRequestType = "board" | "visitorsbook";
+
 export interface VisitorsBookProps {
   _id: string;
   writerid: string;
@@ -18,4 +20,12 @@ export interface VisitorsBookRequestProps {
   bookpassword?: string;
   date: string;
   authtype: boolean;
+}
+
+export interface CommonDeleteRequestProps {
+  _id: string;
+  userid: string;
+  deletetype: string;
+  authtype?: boolean;
+  bookpassword?: string;
 }
