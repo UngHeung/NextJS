c9a65f10fa35c0926b0113c4a0175acf3f87f138
ungHeung <t01047377477@gmail.com>
2023-09-14 16:32:47 +0900

    refactor : 유저 인터페이스 유저속성, 세션 속성 분리

--- utils/interface/user/userInterfaces.ts
+++ utils/interface/user/userInterfaces.ts
@@ -1,12 +1,8 @@
-/**
- * 유저 인터페이스
- */
-
 export interface UserDataProps {
   _id: string;
   name: string;
   email: string;
-  password: string;
+  password?: string;
 }
 
 export type UserSessionProps = Omit<UserDataProps, "password">;
