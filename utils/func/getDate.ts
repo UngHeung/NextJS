ed662bc9e3585f6b946af7f5dc70409b89128772
ungHeung <t01047377477@gmail.com>
2023-09-14 17:09:39 +0900

    feat : 시간, 분이 10보다 적을 경우 앞에 0 붙여주는 기능

--- utils/func/getDate.ts
+++ utils/func/getDate.ts
@@ -5,8 +5,10 @@ const getDate = () => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  let hour = date.getHours().toString();
+  let minute = date.getMinutes().toString();
+  hour = hour.length < 2 ? "0" + hour : hour;
+  minute = minute.length < 2 ? "0" + minute : minute;
 
   let result = `${year}-${month}-${day} ${hour}:${minute}`;
   return result;
