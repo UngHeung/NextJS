71aab5bb2dfba2fe8569e3b531aa6df6d19690c1
ungHeung <t01047377477@gmail.com>
2023-09-18 16:46:34 +0900

    feat : fetch api config

---
+++ pages/api/apiConfig.ts
@@ -0,0 +1,30 @@
+/**
+ * fetch API
+ * @param method
+ * @param apiUrl
+ * @param data
+ */
+
+type MethodType = "POST" | "GET" | "UPDATE" | "DELETE";
+
+const fetchApi = async (method: MethodType, apiUrl: string, data: any) => {
+  const result = await fetch(apiUrl, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.ok) {
+      console.log("요청 성공");
+    } else {
+      console.log("요청 실패");
+    }
+
+    return response;
+  });
+
+  return result;
+};
+
+export default fetchApi;
