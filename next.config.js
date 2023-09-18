99d5305d0cf65ab13524374fa01b0065ac5dbab9
ungHeung <t01047377477@gmail.com>
2023-09-18 19:15:03 +0900

    fix : baseurl 삭제

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_API_URL,
+  // basePath: process.env.NEXT_PUBLIC_API_URL,
 };
 
 module.exports = nextConfig;
