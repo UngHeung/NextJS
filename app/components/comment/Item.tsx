c7a7efef6ed929882515ad152549c821f674006b
ungHeung <t01047377477@gmail.com>
2023-09-19 04:01:05 +0900

    feat : 댓글 수정 기능

--- app/components/comment/Item.tsx
+++ app/components/comment/Item.tsx
@@ -1,21 +1,72 @@
-import React from "react";
-import { CommentProps } from "./Form";
+/**
+ * 댓글 아이템 컴포넌트
+ */
+
+"use client";
+
+import React, { useState } from "react";
+import { CommentProps, CommentUpdateProps } from "./Form";
+import { FormEvent } from "@/utils/interface/eventType";
+import { useRouter } from "next/navigation";
+import getDate from "@/utils/func/getDate";
+import fetchApi from "@/pages/api/apiConfig";
 
 const Item = ({ ...props }: CommentProps) => {
-  const writer = props.writer;
-  const [date, time] = props.date.split(" ");
+  const [itemState, setItemState] = useState(false);
+  const [comment, setCommnet] = useState(props.comment);
+  const router = useRouter();
+
+  const handleCommentUpdate = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data: CommentUpdateProps = {
+      _id: formData.get("commentid") as string,
+      comment: formData.get("comment") as string,
+      postid: formData.get("postid") as string,
+      date: getDate(),
+    };
+
+    try {
+      await fetchApi("POST", "/api/comment/update", data).then((response) => {
+        if (response.ok) {
+          router.refresh();
+          router.push(response.url);
+        }
+      });
+    } catch (e) {
+      console.error("comment_update_서버에 오류 발생\n" + e);
+    }
+  };
 
   return (
     <li>
-      <div>
-        <strong>{writer}</strong>
-        <time dateTime={props.date}>
-          {date}
-          {time}
-        </time>
-      </div>
-      <p>{props.comment}</p>
-      <button>수정</button>
+      <form
+        onSubmit={(e) => {
+          handleCommentUpdate(e);
+          setItemState(false);
+        }}
+      >
+        <strong>{props.writer}</strong>
+        <div style={{ display: "none" }}>
+          <input type="string" name="commentid" defaultValue={props._id} />
+          <input type="string" name="postid" defaultValue={props.postid} />
+        </div>
+        <input type="string" name="date" defaultValue={props.date} />
+        <textarea
+          name="comment"
+          onChange={(e) => setCommnet(e.target.value)}
+          value={comment}
+          readOnly={!itemState}
+        ></textarea>
+        {itemState && <button>저장</button>}
+      </form>
+
+      {itemState ? (
+        <button onClick={() => setItemState(false)}>취소</button>
+      ) : (
+        <button onClick={() => setItemState(true)}>수정</button>
+      )}
       <button>삭제</button>
     </li>
   );
---
+++ pages/api/comment/update.ts
@@ -0,0 +1,32 @@
+/**
+ * 댓글 업데이트 서버 요청
+ */
+
+import getDbCollection from "../getDatabase";
+import { CommentUpdateProps } from "@/app/components/comment/Form";
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const body = req.body as CommentUpdateProps;
+
+  try {
+    await (
+      await getDbCollection("comment")
+    ).updateOne(
+      { _id: new ObjectId(body._id) },
+      {
+        $set: {
+          comment: body.comment,
+          date: body.date,
+        },
+      }
+    );
+
+    res.redirect(302, `/board/detail/${body.postid}`);
+  } catch (e) {
+    console.error("board_update_서버요청 오류 발생\n" + e);
+  }
+};
+
+export default handler;
