33a8ed040018f9e8767ca22fbae153c1b058d0f1
ungHeung <t01047377477@gmail.com>
2023-09-21 10:17:58 +0900

    design : 로고 svg 추가

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import logo from "@/public/assets/logo-dark.svg";
+import Image from "next/image";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { UserSessionProps } from "@/utils/interface/user/userInterfaces";
 import { getServerSession } from "next-auth/next";
@@ -12,7 +14,9 @@ const Header = async () => {
     <header className="main-header">
       <div className="main-header-wrap">
         <h2>
-          <Link href={"/"}>DEV.</Link>
+          <Link href={"/"}>
+            <Image src={logo} alt="로고" width={45} />
+          </Link>
         </h2>
         <nav className="main-nav">
           <ul className="main-menu">
