12c7873f09da69bace3aa396ef15ed40097b52be
ungHeung <t01047377477@gmail.com>
2023-09-23 18:20:01 +0900

    feat : 리드미 메뉴 추가

---
+++ app/readMe/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const readMe = () => {
+  return (
+    <div>
+      <iframe src="https://ungheung.github.io/NextJS/"></iframe>
+    </div>
+  );
+};
+
+export default readMe;
