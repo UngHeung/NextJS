0ec5a635ae3b8a934c200b92f31c7f4429c4a427
ungHeung <t01047377477@gmail.com>
2023-09-24 01:16:58 +0900

    design : 푸터 내용 추가 및 스타일 수정

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="main-footer">
       <div className="main-footer-wrap">
-        <h2>Next.js 연습하기</h2>
+        <strong className="footer-name">Ungheung_ </strong>
+        <span className="line">|</span>
+        <Link href={"https://github.com/UngHeung/NextJS"} target="_blank">
+          GitHub
+        </Link>
+        <span className="line">|</span>
+        <span className="footer-email"> beehive2838@naver.com </span>
+        <span className="line">|</span>
+        <span className="footer-phone">010.1234.4321 </span>
       </div>
     </footer>
   );
